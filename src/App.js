f15a9ce7f3c02fd349076d5379bcaea470496126
srikant <srikantkharsh@gmail.com>
2020-05-11 01:54:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+import PostList from './component/PostList'
+
+function App() {
+  return (
+    <div className="App">
+        <PostList  name= {"srikant"}/>
+    </div>
+  );
+}
+
+export default App;
